f0570c2692e590df309c655bfeb63100aa44a37c
Hadeel Obaid <hadeelobaid71@gmail.com>
2024-03-12 15:27:03 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,3 +1,35 @@
 class Formatter {
   //add static methods here
+  static capitalize(lower) {
+    return lower.charAt(0).toUpperCase() + lower.substring(1);
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9-' ]+/g, "");
+  }
+
+  static titleize(sentence) {
+    const smallWords = [
+      "the",
+      "a",
+      "an",
+      "but",
+      "of",
+      "and",
+      "for",
+      "at",
+      "by",
+      "from",
+    ];
+    return sentence
+      .split(" ")
+      .map((word, index) => {
+        if (index === 0 || !smallWords.includes(word.toLowerCase())) {
+          return word.charAt(0).toUpperCase() + word.slice(1);
+        } else {
+          return word.toLowerCase();
+        }
+      })
+      .join(" ");
+  }
 }
